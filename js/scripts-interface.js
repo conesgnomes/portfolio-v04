af2a12eca1b7f45273a89c8aca0b968fdf1e2482
conesgnomes <connorlarsen@gmail.com>
2019-04-20 00:43:01 -0700

    build out and style daily_ui.html

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -130,3 +130,26 @@ $(document).ready(function () {
       console.log(scroll_pos);
   });
 });
+
+$(document).ready(function() {
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+        /* Check the location of each desired element */
+        $('.hideme1').each( function(i){
+            var bottom_of_object = $(this).position().top + $(this).outerHeight() - 50;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1', 'bottom':'0'},500);
+            }
+        });
+        $('.hideme2').each( function(i){
+            var bottom_of_object = $(this).position().top + $(this).outerHeight() - 50;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},500);
+            }
+        });
+    });
+});
