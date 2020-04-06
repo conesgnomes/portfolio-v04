722e1705829687e3a9021a61ba3b218b804e2ac0
conesgnomes <connorlarsen@gmail.com>
2020-04-06 12:36:14 -0700

    adjust scss in daily_ui.scss

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -280,10 +280,10 @@ $(document).ready(function() {
     $(window).scroll( function(){
         /* Check the location of each desired element */
         $('.hideme1').each( function(i){
-            var bottom_of_object = $(this).position().top + $(this).outerHeight() - 50;
+            var bottom_of_object = $(this).position().top + $(this).outerHeight() - 175;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
+            if( bottom_of_window >= bottom_of_object ){
                 $(this).animate({'opacity':'1', 'bottom':'0'},500);
             }
         });
